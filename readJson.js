e67da316c78bd51a8535c6532ac47ca8de87e038
shekharsvbhosale <shekharsvbhosale@gmail.com>
2017-12-10 23:55:59 +0530

    C1

---
+++ readJson.js
@@ -0,0 +1,18 @@
+//code to call onload function
+//windows.onload...
+
+$(document).ready(function () {
+        // EXTRACT JSON DATA.
+        $.getJSON("http://localhost/custom/domains.json", function (data) 
+        		{debugger;
+            $.each(data, function (index, value) {
+                // APPEND OR INSERT DATA TO SELECT ELEMENT.
+                $('#sel').append('<option value="' + value.domainName + '">' + value.domainName + '</option>');
+            });
+        });
+
+        // SELECT change EVENT TO READ SELECTED VALUE FROM DROPDOWN LIST.
+        $('#sel').change(function () {
+            $('p').text('Selected Item: ' + this.options[this.selectedIndex].text);
+        });
+    });
